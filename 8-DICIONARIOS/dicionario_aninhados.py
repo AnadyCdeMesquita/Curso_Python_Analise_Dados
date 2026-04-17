79d923b7fdab247a8768a68bab03befc17a71a7c
Anady Carvalho <anadyads2022@hotmail.com>
2026-04-17 13:55:54 -0300

    Dicionarios_Aninhados

---
+++ 8-DICIONARIOS/dicionario_aninhados.py
@@ -0,0 +1,11 @@
+alunos = {
+  'Danny':{ 'idade': 20, 'Curso': 'TI'},
+  'Fernando':{ 'idade': 35, 'Curso': 'ADM'},
+  'Rodrigo': { 'idade': 49, 'Curso': 'ENG'}
+  }
+
+alunos['Daniel'] = { 'idade': 20, 'Curso': 'MED'}
+
+print(alunos)
+
+print(alunos['Danny']['Curso'])
